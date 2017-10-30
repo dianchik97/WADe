ab86888eec65910868cb08e94ec19e7eefa91d2c
dianchik97 <dianchik79.lukyanenko@yandex.ru>
2017-10-30 23:15:23 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { MdlModule } from '@angular-mdl/core';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+declarations: [ AppComponent, FooterComponent, HeaderComponent ],
+
   imports: [
-    BrowserModule
+    BrowserModule, MdlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
